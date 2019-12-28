cf018102291472d56a33650f1f4e1cca8916299e
skup5 <5skup5@seznam.cz>
2019-12-29 00:02:04 +0100

    SHAHashTest.kt - added hash test of new generated key.

--- UIClient/src/test/kotlin/cz/zelenikr/remotetouch/security/SHAHashTest.kt
+++ UIClient/src/test/kotlin/cz/zelenikr/remotetouch/security/SHAHashTest.kt
@@ -6,7 +6,12 @@ import kotlin.test.assertEquals
 class SHAHashTest {
 
     @Test
-    fun hash(){
+    fun `hash old generated key`(){
         assertEquals("R7eG00h/U9wVRkiITVOpZWQJT9E1YZmTAIXBuRbNfz4=",SHAHash().hash("XA1[B@6799e8e"))
+    }
+
+    @Test
+    fun `hash new generated key`(){
+        assertEquals("KGuP8oQUhmTHg++1IYODoC8Aa9hSFZYUfimiEC03CdM=", SHAHash().hash("XA1g1JSX7uwb4gmUlmGEe4mFQ=="))
     }
 }
